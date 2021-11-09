9a2f79f34cb22f4b69d997f587c561910a437e29
Saisha Chhabria <48245205+saishachhabria@users.noreply.github.com>
2021-11-09 14:01:05 +0530

    Updated frame extractor and added resize function
    links.txt has the youtube video links to extract frames from

--- data_collection/frame_extractor_yt.py
+++ data_collection/frame_extractor_yt.py
@@ -4,38 +4,41 @@ import youtube_dl
 
 if __name__ == '__main__':
 
-    video_url = 'https://www.youtube.com/watch?v=8wewPn7TZfs'
-    # video_url = 'https://www.youtube.com/watch?v=Xtwar56r4Lg'
-    # video_url = 'https://www.youtube.com/watch?v=c3hh1AYMo7o'
+    with open('links.txt') as file:
+        urls = file.readlines()
     
-    ydl_opts = {}
-    i = 214 # frame count
-    ydl = youtube_dl.YoutubeDL(ydl_opts)
-    info_dict = ydl.extract_info(video_url, download=False)
+    for index, video_url in enumerate(urls):
+        
+        code = video_url[video_url.index('=')+1:]
+    
+        ydl_opts = {}
+        i = 0 # frame count
+        ydl = youtube_dl.YoutubeDL(ydl_opts)
+        info_dict = ydl.extract_info(video_url, download=False)
 
-    formats = info_dict.get('formats',None)
+        formats = info_dict.get('formats',None)
 
-    for f in formats:
+        for f in formats:
 
-        if f.get('format_note',None) == '144p':
-            url = f.get('url',None)
-            cap = cv2.VideoCapture(url)
-            if not cap.isOpened():
-                print('Video not opened')
-                exit(-1)
-            count = 0
-            while True:
-                ret, frame = cap.read()
-                if not ret: break
-                # cv2.imshow('frame', frame)
-                count += 1
-                if count % 60 == 0:
-                    cv2.imwrite('data/frame'+str(i)+'.jpg',frame)
-                    i += 1
-                    count = 0
-                if cv2.waitKey(30)&0xFF == ord('q'): break
+            if f.get('format_note',None) == '144p':
+                url = f.get('url',None)
+                cap = cv2.VideoCapture(url)
+                if not cap.isOpened():
+                    print('Video not opened')
+                    exit(-1)
+                count = 0
+                while True:
+                    ret, frame = cap.read()
+                    if not ret: break
+                    # cv2.imshow('frame', frame)
+                    count += 1
+                    if count % 60 == 0:
+                        file_name = 'data/{}_{}_{}.jpg'.format(index, code, str(i))
+                        cv2.imwrite(file_name, frame)
+                        i += 1
+                        count = 0
+                    if cv2.waitKey(30)&0xFF == ord('q'): break
 
-            # release VideoCapture
-            cap.release()
+                cap.release()
 
-    cv2.destroyAllWindows()
+        cv2.destroyAllWindows()
