f54fe189fedf3e886f2644cbf6ec336081a21f14
Rishabh Sheoran <sheoran95@gmail.com>
2021-11-18 21:52:18 +0530

    CNN Improvement commit 1

--- src/dataset.py
+++ src/dataset.py
@@ -13,41 +13,32 @@ CLASSES = ["chandler", "joey", "monica", "phoebe", "rachel", "ross"]
 
 
 class ImageDataset(Dataset):
-    def __init__(self, train, test):
+    def __init__(self, train):
         labels_df = pd.read_csv("../data/processed/labels.csv")
         rows_with_no_class = labels_df[~(
             labels_df[CLASSES] == 0).all(axis=1)].copy()
         self.csv = rows_with_no_class
         self.train = train
-        self.test = test
         self.all_image_names = self.csv[:]["filename"]
         self.all_labels = np.array(self.csv.drop(["filename", ], axis=1))
-        self.train_ratio = int(0.6 * len(self.csv))
-        self.valid_ratio = int(0.2 * len(self.csv))
-        self.test_ratio = len(self.csv) - self.train_ratio - self.valid_ratio
+        self.train_ratio = int(0.85 * len(self.csv))
+        self.test_ratio = len(self.csv) - self.train_ratio
 
         if self.train == True:
             print(f"Number of training images: {self.train_ratio}")
             self.image_names = list(self.all_image_names[: self.train_ratio])
             self.labels = list(self.all_labels[: self.train_ratio])
 
-        elif self.train == False and self.test == False:
-            print(f"Number of validation images: {self.valid_ratio}")
+        elif self.train == False:
+            print(f"Number of test images: {self.test_ratio}")
             self.image_names = list(
                 self.all_image_names[
-                    self.train_ratio: self.train_ratio + self.valid_ratio
+                    -self.test_ratio:
                 ]
             )
             self.labels = list(
-                self.all_labels[self.train_ratio: self.train_ratio +
-                                self.valid_ratio]
+                self.all_labels[-self.test_ratio:]
             )
-
-        elif self.test == True and self.train == False:
-            print(f"Number of test images: {self.test_ratio}")
-            self.image_names = list(self.all_image_names[-self.test_ratio:])
-            self.labels = list(self.all_labels[-self.test_ratio:])
-
         self.transform = transforms.Compose([transforms.ToTensor(), ])
 
     def __len__(self):
